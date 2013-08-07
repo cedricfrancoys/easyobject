076cae7ee46b21e67db32a0110c8065b17c9c417
cedseven <cedseven@c053c48c-d355-40d2-b402-9763902aefd3>
2013-08-07 16:14:17 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/easyobject/code@38 c053c48c-d355-40d2-b402-9763902aefd3

--- packages/school/classes/Course.class.php
+++ packages/school/classes/Course.class.php
@@ -9,6 +9,7 @@ namespace school {
 				'school_id'		=> array('type' => 'many2one', 'foreign_object' => 'school\School'),
 				'teachers_ids'	=> array('type' => 'many2many', 'foreign_object' => 'school\Teacher', 'foreign_field' => 'courses_ids', 'rel_table' => 'school_rel_course_teacher', 'rel_foreign_key' => 'teacher_id', 'rel_local_key' => 'course_id'),
 				'classes_ids'	=> array('type' => 'one2many', 'foreign_object' => 'school\_Class', 'foreign_field' => 'course_id'),
+				'school_name'	=> array('type' => 'related', 'result_type' => 'string', 'foreign_object' => 'school\School', 'path' => array('school_id', 'name'))
 			);
 		}
 	}
--- packages/school/classes/Lesson.class.php
+++ packages/school/classes/Lesson.class.php
@@ -12,7 +12,10 @@ namespace school {
 				'students_ids'			=> array('type' => 'related', 'result_type' => 'one2many', 'foreign_object' => 'school\Student', 'path' => array('class_id', 'students_ids')),
 				'teacher_courses_ids'	=> array('type' => 'related', 'result_type' => 'one2many', 'foreign_object' => 'school\Course', 'path' => array('class_id', 'teacher_id', 'courses_ids')),
 				'teacher_id'			=> array('type' => 'related', 'result_type' => 'many2one', 'foreign_object' => 'school\Teacher', 'path' => array('class_id', 'teacher_id')),
-				'school_id'				=> array('type' => 'related', 'result_type' => 'many2one', 'foreign_object' => 'school\School', 'path' => array('class_id', 'course_id', 'school_id')),
+				'school_id'				=> array('type' => 'related', 'result_type' => 'many2one', 'foreign_object' => 'school\School', 'path' => array('class_id', 'course_id', 'school_id')),				
+				'class_label'			=> array('type' => 'related', 'result_type' => 'string', 'foreign_object' => 'school\_Class', 'path' => array('class_id', 'label')),
+				'school_name'			=> array('type' => 'related', 'result_type' => 'string', 'foreign_object' => 'school\School', 'path' => array('class_id', 'course_id', 'school_id', 'name')),
+				'teacher_name'			=> array('type' => 'related', 'result_type' => 'string', 'foreign_object' => 'school\Teacher', 'path' => array('class_id', 'teacher_id', 'lastname'))
 			);
 		}
 	}
