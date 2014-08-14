613069c053cca2e5afe1cc3c38db9e7b4d75d999
cedseven <cedseven@c053c48c-d355-40d2-b402-9763902aefd3>
2014-08-14 11:34:24 +0000

    git-svn-id: svn+ssh://svn.code.sf.net/p/easyobject/code@101 c053c48c-d355-40d2-b402-9763902aefd3

--- packages/icway/data/resource.php
+++ packages/icway/data/resource.php
@@ -12,6 +12,14 @@ $params = get_params(array('res_id'=>null, 'mode'=>'view', 'lang'=>'fr'));
 
 $resource_values = &browse('icway\Resource', array($params['res_id']), array('title', 'filename', 'type', 'size', 'content'));
 
+$resource_content = false;
+
+if(BINARY_STORAGE_MODE == 'DB') $resource_content = $resource_values[$params['res_id']]['content'];
+else if(BINARY_STORAGE_MODE == 'FS') {
+	$resource_content = file_get_contents($resource_values[$params['res_id']]['content']);
+}
+if($resource_content === false) die('unable to provide content');
+
 // disable compression whatever default option is
 ini_set('zlib.output_compression','0');
 
@@ -26,13 +34,6 @@ header("Expires: 0");
 header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
 header("Cache-Control: public");
 header("Content-Type: ".$resource_values[$params['res_id']]['type']);
-// todo: ? header('Content-Type: application/x-download');
 header("Content-Length: ".$resource_values[$params['res_id']]['size']);
 
-$resource_content = '';
-
-if(BINARY_STORAGE_MODE == 'DB') $resource_content = $resource_values[$params['res_id']]['content'];
-else if(BINARY_STORAGE_MODE == 'FS') {
-	$resource_content = file_get_contents($resource_values[$params['res_id']]['content']);
-}
 print($resource_content);
